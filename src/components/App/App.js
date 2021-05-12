b0863bcc71a6bca6586b73eeabf4d4f340964575
sem <sem@iMac-Ucenik-3.local>
2021-05-12 13:25:45 +0300

    login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import SelectParty from '../SelectParty/SelectParty';
 import MakeParty from '../MakeParty/MakeParty';
 import PartyMaker from '../PartyMaker/PartyMaker';
+import Login from '../Login/Login';
 import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <Route path="/sign-in">
+        <Login />
+      </Route>
       <Route path="/search">
         <SelectParty />
       </Route>
